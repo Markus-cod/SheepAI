62118673e8207dcf254eb81d110722cbb290cedb
anton <64925492+AntonVucinic@users.noreply.github.com>
2025-11-29 13:54:57 +0100

    Add categories

--- backend/hnplusplus/model/user.py
+++ backend/hnplusplus/model/user.py
@@ -1,5 +1,6 @@
+from __future__ import annotations
 from pydantic import BaseModel
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, Relationship, SQLModel
 
 
 class Token(BaseModel):
@@ -15,9 +16,19 @@ class UserBase(SQLModel):
     username: str = Field(index=True, unique=True)
 
 
+class UserCategoryLink(SQLModel, table=True):
+    user_id: int | None = Field(default=None, foreign_key="user.id", primary_key=True)
+    category_id: int | None = Field(
+        default=None, foreign_key="category.id", primary_key=True
+    )
+
+
 class User(UserBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     hashed_password: str
+    categories: list[Category] = Relationship(
+        back_populates="teams", link_model=UserCategoryLink
+    )
 
 
 class UserPublic(UserBase):
@@ -27,3 +38,8 @@ class UserPublic(UserBase):
 class UserCreate(UserBase):
     username: str
     password: str
+
+
+class Category(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(unique=True)
